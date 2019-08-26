1c35e4b1c9aa2e15d653e91dfb8a37e647301443
Phat <kingdragontp2@gmail.com>
2019-08-26 13:56:51 +0700

    new change

--- nghia.js
+++ nghia.js
@@ -1,10 +1,10 @@
 console.log('hello');
-<<<<<<< refs/remotes/origin/master
 
 ///123456498799
-=======
 console.log('hello 01');
 function hello(){
     console.log("xin chao");
+    let a = 0;
+    let b = 1;
+    console.log(a + b);
 }
->>>>>>> update
